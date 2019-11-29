83db4d917bda0d3b88129e57fc140e430faef102
Marcin Jaskulski <jaskulski.marcin@outlook.com>
2019-11-29 19:06:05 +0100

    Added technical documentation to: -ScenarioElementsModel -ScenarioJSONModel -ScenarioStepCounter -ScenarioStepCounterVisitor

--- src/main/java/pl/put/poznan/sqc/models/ScenarioJSONModel.java
+++ src/main/java/pl/put/poznan/sqc/models/ScenarioJSONModel.java
@@ -3,15 +3,33 @@ import pl.put.poznan.sqc.elements.Title;
 
 import java.util.ArrayList;
 
+/**
+ * Storage JSON scenario model.
+ */
 public class ScenarioJSONModel {
+    /**
+     * Constructor: Initialize ActorsList and ScenarioLinesList.
+     */
     public ScenarioJSONModel() {
         ActorsList = new ArrayList<String>();
         ScenarioLinesList = new ArrayList<String>();
     }
 
+    /**
+     * Stores the Title.
+     */
     public String Title;
+    /**
+     * Stores the ActorsList.
+     */
     public ArrayList<String> ActorsList;
+    /**
+     * Stores the SystemActor.
+     */
     public String SystemActor;
+    /**
+     * Stores the ScenarioLinesList.
+     */
     public ArrayList<String> ScenarioLinesList;
 
 }
