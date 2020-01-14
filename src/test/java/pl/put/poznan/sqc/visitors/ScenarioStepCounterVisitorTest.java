3adc019bc7eb004c00b090abfcd0bc4c8afa657c
Marcin Jaskulski <jaskulski.marcin@outlook.com>
2020-01-14 13:53:26 +0100

    more tests

--- src/test/java/pl/put/poznan/sqc/visitors/ScenarioStepCounterVisitorTest.java
+++ src/test/java/pl/put/poznan/sqc/visitors/ScenarioStepCounterVisitorTest.java
@@ -26,6 +26,16 @@ public class ScenarioStepCounterVisitorTest {
         visitor.Visit(title);
         assertEquals(1,visitor.numberOfSteps);
     }
+
+    @Test
+    public void testTitleNull(){
+        Title title = new Title();
+        title.title = "";
+
+        visitor.Visit(title);
+        assertEquals(0,visitor.numberOfSteps);
+    }
+
     @Test
     public void testNormalActors(){
         NormalActor normalActor = new NormalActor();
@@ -34,6 +44,16 @@ public class ScenarioStepCounterVisitorTest {
         visitor.Visit(normalActor);
         assertEquals(1,visitor.numberOfSteps);
     }
+
+    @Test
+    public void testNormalActorsNull(){
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "";
+
+        visitor.Visit(normalActor);
+        assertEquals(0,visitor.numberOfSteps);
+    }
+
     @Test
     public void testMoreNormalActors(){
         NormalActor normalActor = new NormalActor();
@@ -54,6 +74,23 @@ public class ScenarioStepCounterVisitorTest {
         assertEquals(1,visitor.numberOfSteps);
     }
     @Test
+    public void testSystemActorNull(){
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "";
+
+        visitor.Visit(systemActor);
+        assertEquals(0,visitor.numberOfSteps);
+    }
+    @Test
+    public void testScenarioLineNull(){
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+        visitor.Visit(scenarioLine);
+        System.out.println(visitor.numberOfSteps);
+        assertEquals(0,visitor.numberOfSteps);
+    }
+    @Test
     public void testScenarioLine(){
         ScenarioLine scenarioLine= new ScenarioLine();
         scenarioLine.scenarioLine =" some random words";
@@ -61,6 +98,15 @@ public class ScenarioStepCounterVisitorTest {
         visitor.Visit(scenarioLine);
         System.out.println(visitor.numberOfSteps);
         assertEquals(1,visitor.numberOfSteps);
+    }
+    @Test
+    public void testTwoScenarioLine(){
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine =" some random words";
+
+        visitor.Visit(scenarioLine);
+        visitor.Visit(scenarioLine);
+        assertEquals(2,visitor.numberOfSteps);
     }
     @Test
     public void testMoreScenarioLine(){
@@ -71,5 +117,162 @@ public class ScenarioStepCounterVisitorTest {
         visitor.Visit(scenarioLine);
         visitor.Visit(scenarioLine);
         assertEquals(3,visitor.numberOfSteps);
+    }
+
+    @Test
+    public void testTitleAndNormalActors(){
+        Title title = new Title();
+        title.title = "Dodanie ksiazki";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Janek";
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        assertEquals(2,visitor.numberOfSteps);
+    }
+
+    @Test
+    public void testTitleAndNormalActorsAndSystemActors(){
+        Title title = new Title();
+        title.title = "Dodanie ksiazki";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Janek";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "System";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        assertEquals(3,visitor.numberOfSteps);
+    }
+
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine(){
+        Title title = new Title();
+        title.title = "Dodanie ksiazki";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Janek";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "System";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine =" some random words";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(4,visitor.numberOfSteps);
+    }
+
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine2(){
+        Title title = new Title();
+        title.title = "Dodanie ksiazki";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Janek";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "System";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(3,visitor.numberOfSteps);
+    }
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine3(){
+        Title title = new Title();
+        title.title = "Dodanie ksiazki";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "System";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(2,visitor.numberOfSteps);
+    }
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine4(){
+        Title title = new Title();
+        title.title = "";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "System";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(1,visitor.numberOfSteps);
+    }
+
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine5(){
+        Title title = new Title();
+        title.title = "";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(0,visitor.numberOfSteps);
+    }
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine6(){
+        Title title = new Title();
+        title.title = "";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Actor";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(1,visitor.numberOfSteps);
+    }
+    @Test
+    public void testTitleAndNormalActorsAndSystemActorsAndLine7(){
+        Title title = new Title();
+        title.title = "";
+        NormalActor normalActor = new NormalActor();
+        normalActor.normalActor = "Actor";
+        SystemActor systemActor = new SystemActor();
+        systemActor.systemActor = "";
+        ScenarioLine scenarioLine= new ScenarioLine();
+        scenarioLine.scenarioLine ="Linia";
+
+
+        visitor.Visit(title);
+        visitor.Visit(normalActor);
+        visitor.Visit(systemActor);
+        visitor.Visit(scenarioLine);
+        assertEquals(2,visitor.numberOfSteps);
     }
 }
